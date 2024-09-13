11c23ebee35f42a6be945341a19b7e9a9874ef21
Ezhil-Sekar-ICD201-01 <166552821+Ezhil-Sekar-ICD201-01@users.noreply.github.com>
2024-09-13 14:26:36 +0000

    Answer

--- PP1_1.py
+++ PP1_1.py
@@ -1,5 +1,5 @@
 def q1():
-  print("Hello world")
+  print("Hello World")
 def q2():
   #Write Assignment code here
   print(1)
@@ -12,7 +12,7 @@ def q3():
   print(" Help Me!")
 def q4():
   #Write Assignment code here
-  print("3 x 3 = " , 3*3)
+  print("3 x 3 = 9")
 
 q1()
 q2()
